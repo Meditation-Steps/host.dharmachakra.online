67b91dafcee225626df5f3015131bbb1cea64618
amalendu <alexander.berezin@meditation-steps.eu>
2026-05-18 10:31:01 +0200

    Introducing Sveltia CMS

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -14,26 +14,6 @@ export default function IndexPage() {
         <h1 className="page-title font-title">{t("index.title")}</h1>
         <p className="page-subtitle font-regular">{t("index.subtitle")}</p>
       </header>
-
-      <div className="page-main">
-        <main className="page-content">
-          <p className="font-regular" style={{ whiteSpace: "pre-line" }}>
-            {t("index.content.p1")}
-          </p>
-          <p
-            className="font-mantra"
-            style={{
-              textAlign: "center",
-              margin: "1.5rem 0",
-            }}
-          >
-            <strong>{t("index.content.button")}</strong>
-          </p>
-          <p className="font-regular" style={{ whiteSpace: "pre-line" }}>
-            {t("index.content.p2")}
-          </p>
-        </main>
-      </div>
     </div>
   );
 }
